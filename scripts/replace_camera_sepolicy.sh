7ff1db8d8b970030dc224fe070d65cfddd4cd2dd
Anoosragh A R <83896147+anoosragh69@users.noreply.github.com>
2024-01-18 19:31:52 +0530

    Nuke confimation line

--- scripts/replace_camera_sepolicy.sh
+++ scripts/replace_camera_sepolicy.sh
@@ -3,7 +3,6 @@
 # Run as follows, ./device/motorola/targets/scripts/replace_camera_sepolicy.sh
 
 shopt -s extglob
-read -p "Please run this script at the root of your synced source!, Ctrl-C to exit at this point, Enter to continue."
 find device/!(*qcom)/sepolicy -type f -exec sed -i "/vendor.camera.aux.packagelist/s/.*//" {} \;
 if [ $? -eq 0 ]; then
 	echo "Patch 1, Done!"
