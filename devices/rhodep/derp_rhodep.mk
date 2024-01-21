2d20330e49d47be7daa80b8e11bcd41e432c1f76
Anoosragh <ok.ano0s6090@gmail.com>
2024-01-21 11:41:13 +0000

    Fix it soon lol
    Signed-off-by: Anoosragh <ok.ano0s6090@gmail.com>

--- devices/rhodep/derp_rhodep.mk
+++ devices/rhodep/derp_rhodep.mk
@@ -1,1 +1,24 @@
-../../rom/derp.mk
+# Copyright 2014 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Common
+include device/motorola/targets/include/common.mk
+
+# Inherit some common derpfest stuff.
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
+
+# Kernel
+$(call inherit-product, device/motorola/targets/include/kernel/source.mk)
+
+PRODUCT_NAME := derp_$(DEVICE)
